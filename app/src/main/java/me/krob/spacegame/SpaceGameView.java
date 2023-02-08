1809dd5ed729684c8924f2888928d920447759de
kylerob25 <kylrob25@icloud.com>
2023-02-08 11:17:16 +0000

    initial commit

---
+++ app/src/main/java/me/krob/spacegame/SpaceGameView.java
@@ -0,0 +1,226 @@
+package me.krob.spacegame;// package com.example.glenn.spacegame;
+
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+public class SpaceGameView extends SurfaceView implements Runnable{
+
+    private Context context;
+
+    // This is our thread
+    private Thread gameThread = null;
+
+    // Our SurfaceHolder to lock the surface before we draw our graphics
+    private SurfaceHolder ourHolder;
+
+    // A boolean which we will set and unset
+    // when the game is running- or not.
+    private volatile boolean playing;
+
+    // Game is paused at the start
+    private boolean paused = true;
+
+    // A Canvas and a Paint object
+    private Canvas canvas;
+    private Paint paint;
+
+    // This variable tracks the game frame rate
+    private long fps;
+
+    // This is used to help calculate the fps
+    private long timeThisFrame;
+
+    // The size of the screen in pixels
+    private int screenX;
+    private int screenY;
+
+    // The score
+    public int score = 0;
+
+    // Lives
+    private int lives = 4;
+    Spaceship spaceShip;
+
+    // This special constructor method runs
+    public SpaceGameView(Context context, int x, int y) {
+
+        // The next line of code asks the
+        // SurfaceView class to set up our object.
+        // How kind.
+        super(context);
+
+        // Make a globally available copy of the context so we can use it in another method
+        this.context = context;
+
+        // Initialize ourHolder and paint objects
+        ourHolder = getHolder();
+        paint = new Paint();
+
+        screenX = x;
+        screenY = y;
+
+
+
+        initLevel();
+    }
+
+
+
+    private void initLevel(){
+
+        spaceShip = new Spaceship(context, screenX, screenY);
+
+    }
+
+
+    @Override
+    public void run() {
+        while (playing) {
+        score = 10;
+            // Capture the current time in milliseconds in startFrameTime
+            long startFrameTime = System.currentTimeMillis();
+
+            // Update the frame
+            if(!paused){
+                update();
+            }
+
+            // Draw the frame
+            draw();
+
+            // Calculate the fps this frame
+            // We can then use the result to
+            // time animations and more.
+            timeThisFrame = System.currentTimeMillis() - startFrameTime;
+            if (timeThisFrame >= 1) {
+                fps = 1000 / timeThisFrame;
+            }
+
+        }
+
+    }
+
+
+
+    private void update(){
+
+        spaceShip.update(fps);
+        checkCollisions();
+
+    }
+
+
+    private void checkCollisions(){
+      
+	  
+
+    }
+
+
+
+
+    private void draw(){
+        // Make sure our drawing surface is valid or we crash
+        if (ourHolder.getSurface().isValid()) {
+            // Lock the canvas ready to draw
+            canvas = ourHolder.lockCanvas();
+
+            // Draw the background color
+            canvas.drawColor(Color.argb(255, 26, 128, 182));
+
+            // Choose the brush color for drawing
+            paint.setColor(Color.argb(255,  255, 255, 255));
+
+            //  draw the defender
+            canvas.drawBitmap(spaceShip.getBitmap(), spaceShip.getX(), spaceShip.getY() , paint);
+
+
+
+
+            // Draw the score and remaining lives
+            // Change the brush color
+            paint.setColor(Color.argb(255,  249, 129, 0));
+            paint.setTextSize(40);
+            canvas.drawText("Score: " + score + "   Lives: " + lives, 10,50, paint);
+
+            // Draw everything to the screen
+            ourHolder.unlockCanvasAndPost(canvas);
+        }
+    }
+
+
+
+
+    // If SpaceGameActivity is paused/stopped
+    // shutdown our thread.
+    public void pause() {
+        playing = false;
+        try {
+            gameThread.join();
+        } catch (InterruptedException e) {
+            Log.e("Error:", "joining thread");
+        }
+
+    }
+
+
+
+    // If SpaceInvadersActivity is started then
+    // start our thread.
+    public void resume() {
+        playing = true;
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+
+    // The SurfaceView class implements onTouchListener
+    // So we can override this method and detect screen touches.
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+
+        switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+
+            // Player has touched the screen
+
+            case MotionEvent.ACTION_DOWN:
+
+                paused = false;
+
+				// *******************************************
+                // Put space ship movement control codes here
+				// *******************************************
+
+                break;
+
+            // Player has removed finger from screen
+            case MotionEvent.ACTION_UP:
+
+				// *******************************************
+				// Put space ship movement control codes here
+				// *******************************************
+			 
+                break;
+        }
+        return true;
+    }
+
+
+
+
+
+}  // end class
